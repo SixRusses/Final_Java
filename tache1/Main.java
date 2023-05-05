8d9b3fbdb246cf7ee34eb4f86d8b2d1419a4f656
nyftheart <alexandre.kramer@ynov.com>
2023-05-05 09:49:58 +0200

    tache 1 up

--- tache1/Main.java
+++ tache1/Main.java
@@ -10,18 +10,18 @@ public class Main {
         list.addFirst(1);
         list.addFirst(0);
 
-        System.out.println(list.contains(5)); // true
-        System.out.println(list.contains(12)); // false
+        System.out.println(list.contains(5));
+        System.out.println(list.contains(12));
 
-        System.out.println(list.indexOf(10)); // 2
-        System.out.println(list.indexOf(3)); // -1
+        System.out.println(list.indexOf(10));
+        System.out.println(list.indexOf(3));
 
         list.deleteFirst();
 
         list.deleteLast();
 
-        System.out.println(list.contains(0)); // false
+        System.out.println(list.contains(0));
 
-        System.out.println(list.contains(15)); // true
+        System.out.println(list.contains(15));
     }
 }
