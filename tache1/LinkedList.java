e5ec74ecbf61fb56bfe2a3e819098ccaf3d42f1f
nyftheart <alexandre.kramer@ynov.com>
2023-05-05 09:22:20 +0200

    add new

---
+++ tache1/LinkedList.java
@@ -0,0 +1,14 @@
+package m1.examen_final.tache1;
+
+public class LinkedList {
+    private Node first; //can be called head
+    private Node last; //can be called tail
+
+
+    // addFirst
+    // addLast
+    // deleteFirst
+    // deleteLast
+    // contains
+    // indexOf
+}
---
+++ tache1/Node.java
@@ -0,0 +1,6 @@
+package m1.examen_final.tache1;
+
+public class Node {
+    private int value;
+    private Node next;
+}
